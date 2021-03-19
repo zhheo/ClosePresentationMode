fe20eb3857bd9250d1acd810d098e4f69c3012d6
张洪Heo <qewan@foxmail.com>
2021-03-19 10:14:16 +0800

    Initial Commit

---
+++ Shared/ClosePresentationModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosePresentationModeApp.swift
+//  Shared
+//
+//  Created by 张洪Heo on 2021/3/19.
+//
+
+import SwiftUI
+
+@main
+struct ClosePresentationModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
